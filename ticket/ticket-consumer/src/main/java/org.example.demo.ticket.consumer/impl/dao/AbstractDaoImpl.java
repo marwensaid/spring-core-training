2648553afe2c14af000048f03e80e4aee8bb9155
DESKTOP-61AN8O3\Flavien <11834921+galilip@users.noreply.github.com>
2018-12-12 14:14:49 +0100

    feat(consumer): DAO Configuration
    inject and named

--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/AbstractDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/AbstractDaoImpl.java
@@ -1,9 +1,13 @@
 package org.example.demo.ticket.consumer.impl.dao;
 
 import javax.sql.DataSource;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 public abstract class AbstractDaoImpl {
 
+    @Inject
+    @Named("dataSourceTicket")
     private DataSource dataSource;
 
     protected DataSource getDataSource() {
