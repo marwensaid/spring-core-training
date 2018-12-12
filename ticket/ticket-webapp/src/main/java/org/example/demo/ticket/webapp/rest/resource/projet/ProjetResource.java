e15da01a1115414af71166e68685f7d71cddf301
Etienne Laskar <etienne.laskar74@gmail.com>
2018-12-12 09:27:21 +0100

    version reignier

--- ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/rest/resource/projet/ProjetResource.java
+++ ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/rest/resource/projet/ProjetResource.java
@@ -7,7 +7,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.example.demo.ticket.business.manager.ProjetManager;
+import org.example.demo.ticket.business.contract.ManagerFactory;
+import org.example.demo.ticket.business.manager.impl.ProjetManagerImpl;
 import org.example.demo.ticket.model.bean.projet.Projet;
 import org.example.demo.ticket.model.exception.NotFoundException;
 
@@ -21,6 +22,7 @@ import org.example.demo.ticket.model.exception.NotFoundException;
 @Produces(MediaType.APPLICATION_JSON)
 public class ProjetResource {
 
+    private ManagerFactory managerFactory = ManagerFactory.getInstance();
     /**
      * Renvoie le {@link Projet} d'identifiant {@code pId}
      *
@@ -31,7 +33,7 @@ public class ProjetResource {
     @GET
     @Path("{id}")
     public Projet get(@PathParam("id") Integer pId) throws NotFoundException {
-        ProjetManager vProjetManager = new ProjetManager();
+        ProjetManagerImpl vProjetManager = new ProjetManagerImpl();
         Projet vProjet = vProjetManager.getProjet(pId);
         return vProjet;
     }
@@ -44,7 +46,7 @@ public class ProjetResource {
      */
     @GET
     public List<Projet> get() {
-        ProjetManager vProjetManager = new ProjetManager();
+        ProjetManagerImpl vProjetManager = new ProjetManagerImpl();
         List<Projet> vListProjet = vProjetManager.getListProjet();
         return vListProjet;
     }
