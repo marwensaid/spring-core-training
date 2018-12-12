2606e735e2d025c233c2da1b99544d874e18598c
DESKTOP-61AN8O3\Flavien <11834921+galilip@users.noreply.github.com>
2018-12-12 15:41:20 +0100

    feat(consumer): add method in TicketDAO interface

--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/contract/dao/TicketDao.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/contract/dao/TicketDao.java
@@ -1,9 +1,12 @@
 package org.example.demo.ticket.consumer.contract.dao;
 
+import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
+
 /**
  * Interface DAO du package
  * {@link org.example.demo.ticket.consumer.contract.dao}
  */
 public interface TicketDao {
-    // ...
+
+    int getCountTicket(RechercheTicket pRechercheTicket);
 }
