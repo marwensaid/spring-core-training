ed0558a108da75714feed9b2bbbf33d252b620b1
ngassier <gassiernicolas@gmail.com>
2018-12-12 13:59:44 +0100

    config DAO

---
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/contract/impl/dao/TicketDAO.java
@@ -0,0 +1,4 @@
+package org.example.demo.ticket.consumer.contract.impl.dao;
+
+public interface TicketDAO {
+}
---
+++ ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/dao/TicketDaoImpl.java
@@ -0,0 +1,6 @@
+package org.example.demo.ticket.consumer.impl.dao;
+
+import org.example.demo.ticket.consumer.contract.impl.dao.TicketDAO;
+
+public class TicketDaoImpl extends AbstractDaoImpl implements TicketDAO {
+}
