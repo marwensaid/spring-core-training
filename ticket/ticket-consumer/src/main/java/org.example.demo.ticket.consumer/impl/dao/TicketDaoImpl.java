c70bd4e930e9f1df6ff939b597a8320432e09fc2
DESKTOP-61AN8O3\Flavien <11834921+galilip@users.noreply.github.com>
2018-12-12 15:42:31 +0100

    feat(consumer): add getCountTicket method in TicketDaoImpl

--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/TicketDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/TicketDaoImpl.java
@@ -1,13 +1,21 @@
 package org.example.demo.ticket.consumer.impl.dao;
 
-import org.example.demo.ticket.consumer.contract.dao.TicketDao;
-
 import javax.inject.Named;
+import org.example.demo.ticket.consumer.contract.dao.TicketDao;
+import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-/**
- * Classe d'implémentation de {@link TicketDao}.
- */
+
 @Named
 public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
-    // ...
+
+    @Override
+    public int getCountTicket(RechercheTicket pRechercheTicket) {
+        JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
+        int vNbrTicket = vJdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM ticket",
+                Integer.class);
+
+        return vNbrTicket;
+    }
 }
