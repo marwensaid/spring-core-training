0aec8dda060f02f169dd2f70907a56a0a0f9d5a0
DESKTOP-61AN8O3\Flavien <11834921+galilip@users.noreply.github.com>
2018-12-12 14:50:07 +0100

    fix: missing import

--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/TicketDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/TicketDaoImpl.java
@@ -2,6 +2,8 @@ package org.example.demo.ticket.consumer.impl.dao;
 
 import org.example.demo.ticket.consumer.contract.dao.TicketDao;
 
+import javax.inject.Named;
+
 /**
  * Classe d'implémentation de {@link TicketDao}.
  */
