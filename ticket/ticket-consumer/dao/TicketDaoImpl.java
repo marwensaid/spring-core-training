c2f25e92dab1c3f241686f0b79b70c6e51ae8d34
lydiadekkiche <lydiadekkiche@gmail.com>
2018-12-12 06:51:47 -0800

    spring

---
+++ ticket/ticket-consumer/dao/TicketDaoImpl.java
@@ -0,0 +1,10 @@
+package org.example.demo.ticket.consumer.impl.dao;
+
+
+/**
+ * Classe d'implémentation de {@link TicketDao}.
+ */
+@Named
+public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
+    // ...
+}
