3e9fd4cdebe1d7a2eebfaf2c97637bccdb31677b
Jeremy Bouville <jeremy.bouville@etu.unice.fr>
2018-12-12 07:52:48 -0500

    premier commit apres branch

---
+++ ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/dao/TicketDaoImpl.java
@@ -0,0 +1,6 @@
+package org.example.demo.ticket.consumer.impl.dao;
+
+import org.example.demo.ticket.consumer.contract.dao.TicketDao;
+
+public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
+}
