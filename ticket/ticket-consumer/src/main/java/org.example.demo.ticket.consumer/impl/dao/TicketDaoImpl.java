341e0f4f4b96ccf0b43ed042985ab355c695cbd1
DESKTOP-61AN8O3\Flavien <11834921+galilip@users.noreply.github.com>
2018-12-12 14:11:50 +0100

    feat(consumer): add AbstractDaoImpl

---
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/TicketDaoImpl.java
@@ -0,0 +1,11 @@
+package org.example.demo.ticket.consumer.impl.dao;
+
+import org.example.demo.ticket.consumer.contract.dao.TicketDao;
+
+/**
+ * Classe d'implémentation de {@link TicketDao}.
+ */
+@Named
+public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
+    // ...
+}
