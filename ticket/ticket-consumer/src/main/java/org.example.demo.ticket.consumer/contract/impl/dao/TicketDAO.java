7f09dfff544c33e9b006d7f4441559ba7ecaec70
ngassier <gassiernicolas@gmail.com>
2018-12-12 14:43:18 +0100

    requete sql

--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/contract/impl/dao/TicketDAO.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/contract/impl/dao/TicketDAO.java
@@ -1,4 +1,13 @@
 package org.example.demo.ticket.consumer.contract.impl.dao;
 
-public interface TicketDAO {
+import org.example.demo.ticket.model.bean.ticket.TicketStatut;
+import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
+
+import java.util.List;
+
+public interface TicketDao {
+
+    int getCountTicket(RechercheTicket pRechercheTicket);
+
+    List<TicketStatut> getListStatut();
 }
--- ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/dao/TicketDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/dao/TicketDaoImpl.java
@@ -1,6 +1,74 @@
 package org.example.demo.ticket.consumer.impl.dao;
 
-import org.example.demo.ticket.consumer.contract.impl.dao.TicketDAO;
+import org.example.demo.ticket.consumer.contract.impl.dao.TicketDao;
 
-public class TicketDaoImpl extends AbstractDaoImpl implements TicketDAO {
+import javax.inject.Named;
+
+import org.example.demo.ticket.model.bean.ticket.TicketStatut;
+import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+@Named
+public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
+
+    @Override
+    public int getCountTicket(RechercheTicket pRechercheTicket) {
+        MapSqlParameterSource vParams = new MapSqlParameterSource();
+
+        StringBuilder vSQL = new StringBuilder("SELECT COUNT(*) FROM ticket WHERE 1=1");
+
+        if (pRechercheTicket != null) {
+            if (pRechercheTicket.getAuteurId() != null) {
+                vSQL.append(" AND auteur_id = :auteur_id");
+                vParams.addValue("auteur_id", pRechercheTicket.getAuteurId());
+            }
+            if (pRechercheTicket.getProjetId() != null) {
+                vSQL.append(" AND projet_id = :projet_id");
+                vParams.addValue("projet_id", pRechercheTicket.getProjetId());
+            }
+        }
+
+        NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+        int vNbrTicket = vJdbcTemplate.queryForObject(vSQL.toString(), vParams, Integer.class);
+
+        return vNbrTicket;
+    }
+
+    @Override
+    public List<TicketStatut> getListStatut() {
+        String vSQL = "SELECT * FROM public.statut";
+
+        JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
+
+        RowMapper<TicketStatut> vRowMapper = new RowMapper<TicketStatut>() {
+            public TicketStatut mapRow(ResultSet pRS, int pRowNum) throws SQLException {
+                TicketStatut vTicketStatut = new TicketStatut(pRS.getInt("id"));
+                vTicketStatut.setLibelle(pRS.getString("libelle"));
+                return vTicketStatut;
+            }
+        };
+
+        List<TicketStatut> vListStatut = vJdbcTemplate.query(vSQL, vRowMapper);
+
+        return vListStatut;
+    }
+
+    public void updateTicketStatut(TicketStatut pTicketStatut) {
+        String vSQL = "UPDATE statut SET libelle = :libelle WHERE id = :id";
+
+        SqlParameterSource vParams = new BeanPropertySqlParameterSource(pTicketStatut);
+
+        NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+        int vNbrLigneMaJ = vJdbcTemplate.update(vSQL, vParams);
+    }
 }
