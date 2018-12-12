77145285fc1934a7575b5bbbac090d294650ff9e
Sappa Valentin <sappavalentin@gmail.com>
2018-12-12 13:55:49 +0100

    Premier commit

---
+++ ticket/ticket-business/src/main/java/org/example/demo/ticket/business/manager/impl/ManagerFactoryImpl.java
@@ -0,0 +1,39 @@
+package org.example.demo.ticket.business.manager.impl;
+
+
+import org.example.demo.ticket.business.contract.ManagerFactory;
+
+import javax.inject.Inject;
+
+public class ManagerFactoryImpl implements ManagerFactory {
+
+    @Inject
+    private ProjetManagerImpl projetManager;
+
+    @Inject
+    private TicketManagerImpl ticketManager;
+
+    public ManagerFactoryImpl(){
+        super();
+    }
+
+    @Override
+    public ProjetManagerImpl getProjetManager(){
+        return projetManager;
+    }
+
+    @Override
+    public TicketManagerImpl getTicketManager(){
+        return new TicketManagerImpl();
+    }
+
+    @Override
+    public void setProjetManager(ProjetManagerImpl pProjetManager){
+        projetManager = pProjetManager;
+    }
+
+    @Override
+    public void setTicketManager(TicketManagerImpl pTicketManager) {
+        ticketManager = pTicketManager;
+    }
+}
