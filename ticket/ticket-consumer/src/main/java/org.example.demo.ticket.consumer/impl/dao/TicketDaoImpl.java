fe03572856a3a95efabc91d965c49f2bc1d4a89d
DESKTOP-61AN8O3\Flavien <11834921+galilip@users.noreply.github.com>
2018-12-12 15:45:00 +0100

    feat(consumer): GetCountTicket improvment

--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/TicketDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/impl/dao/TicketDaoImpl.java
@@ -3,7 +3,8 @@ package org.example.demo.ticket.consumer.impl.dao;
 import javax.inject.Named;
 import org.example.demo.ticket.consumer.contract.dao.TicketDao;
 import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 
 @Named
@@ -11,10 +12,23 @@ public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
 
     @Override
     public int getCountTicket(RechercheTicket pRechercheTicket) {
-        JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
-        int vNbrTicket = vJdbcTemplate.queryForObject(
-                "SELECT COUNT(*) FROM ticket",
-                Integer.class);
+        MapSqlParameterSource vParams = new MapSqlParameterSource();
+
+        StringBuilder vSQL = new StringBuilder("SELECT COUNT(*) FROM ticket WHERE 1=1");
+
+        if (pRechercheTicket != null) {
+            if (pRechercheTicket.getAuteurId() != null) {
+                vSQL.append(" AND auteur_id = :auteur_id");
+                vParams.addValue("auteur_id", pRechercheTicket.getAuteurId());
+            }
+            if (pRechercheTicket.getProjetId() != null) {
+                vSQL.append(" AND projet_id = :projet_id");
+                vParams.addValue("projet_id", pRechercheTicket.getProjetId());
+            }
+        }
+
+        NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+        int vNbrTicket = vJdbcTemplate.queryForObject(vSQL.toString(), vParams, Integer.class);
 
         return vNbrTicket;
     }
