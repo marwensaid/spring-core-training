1dd5e34ba75201011194053e0c0cd8151e2c002e
Maxime Brocourt <maxime.brocourt@etu.unice.fr>
2018-12-14 09:24:57 +0100

    End SQL transaction + Exception with Lo4J

---
+++ ticket/model/bean/org/example/demo/ticket/model/bean/ticket/TicketStatut.java
@@ -0,0 +1,14 @@
+package org.example.demo.ticket.model.bean.ticket;
+
+public class TicketStatut {
+    private Integer id;
+    private String libelle;
+
+    public Integer getId() {
+        return id;
+    }
+    public String getLibelle() {
+        return libelle;
+    }
+    //...
+}
--- ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/Impl/TicketDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/Impl/TicketDaoImpl.java
@@ -1,25 +1,29 @@
 package org.example.demo.ticket.consumer.Impl;
 
 import javax.inject.Named;
+import javax.xml.transform.ErrorListener;
 
 import org.example.demo.ticket.consumer.Impl.AbstractDaoImpl;
 import org.example.demo.ticket.consumer.contract.TicketDao;
 import org.example.demo.ticket.model.bean.ticket.TicketStatut;
 import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-
+import org.apache.log4j.Logger;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.List;
-
 
 @Named
 public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
+
+    final static Logger LOGGER = Logger.getLogger(TicketDaoImpl.class);
+
     @Override
     public int getCountTicket(RechercheTicket pRechercheTicket) {
         MapSqlParameterSource vParams = new MapSqlParameterSource();
@@ -66,9 +70,26 @@ public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
     public void updateTicketStatut(TicketStatut pTicketStatut) {
         String vSQL = "UPDATE statut SET libelle = :libelle WHERE id = :id";
 
-        SqlParameterSource vParams = new BeanPropertySqlParameterSource(pTicketStatut);
+        MapSqlParameterSource vParams = new MapSqlParameterSource();
+        vParams.addValue("id", pTicketStatut.getId(), Types.INTEGER);
+        vParams.addValue("libelle", pTicketStatut.getLibelle(), Types.VARCHAR);
 
         NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
         int vNbrLigneMaJ = vJdbcTemplate.update(vSQL, vParams);
+    }
+
+    public void insertTicketStatut(TicketStatut pTicketStatut) {
+        String vSQL = "INSERT INTO statut (id, libelle) VALUES (:id, :libelle)";
+        NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+
+        BeanPropertySqlParameterSource vParams = new BeanPropertySqlParameterSource(pTicketStatut);
+        vParams.registerSqlType("id", Types.INTEGER);
+        vParams.registerSqlType("libelle", Types.VARCHAR);
+
+        try {
+            vJdbcTemplate.update(vSQL, vParams);
+        } catch (DuplicateKeyException vEx) {
+            LOGGER.error("Le TicketStatut existe déjà ! id=" + pTicketStatut.getId(), vEx);
+        }
     }
 }
